15b6b7702cb1873d7e459044e5ae0b4d16832e49
oliver <oliver@localhost.localdomain>
2017-06-20 09:24:29 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from __future__ import absolute_import
 from setuptools import setup, find_packages
 #from distutils.core import setup
 
-setup(name=u'atomicform',
-      version=u'1.0',
-      packages = find_packages(u'.'),
-      package_dir={u'atomicform': u'atomicform'},
+setup(name='atomicform',
+      version='1.0',
+      packages = find_packages('.'),
+      package_dir={'atomicform': 'atomicform'},
       package_data={
-            u'atomicform': [u'data/*'],
+            'atomicform': ['data/*'],
          },
       zip_safe = False
       )
